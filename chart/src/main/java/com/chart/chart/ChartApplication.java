65e577761b5fff38556818878e08f9ea9024103d
lama nawaraj <lama.nawraj00@gmail.com>
2022-01-29 23:59:21 +0900

    displayed charts through client side

--- chart/src/main/java/com/chart/chart/ChartApplication.java
+++ chart/src/main/java/com/chart/chart/ChartApplication.java
@@ -2,9 +2,10 @@ package com.chart.chart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class ChartApplication{
+@SpringBootApplication(scanBasePackages = {"com.chart"})
+public class ChartApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChartApplication.class, args);
