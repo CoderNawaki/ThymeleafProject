c372c286989acfa683742a4a9a8a2ffd7cea871a
lama nawaraj <lama.nawraj00@gmail.com>
2022-01-31 12:20:20 +0900

    added form application

---
+++ forms/src/main/java/com/dev/domain/User.java
@@ -0,0 +1,55 @@
+package com.dev.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String givenName;
+    private String familyName;
+
+    protected User() {
+
+    }
+
+    public User(String givenName, String familyName) {
+        this.givenName = givenName;
+        this.familyName = familyName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    @Override
+    public String toString() {
+        return "User [familyName=" + familyName + ", givenName=" + givenName + ", id=" + id + "]";
+    }
+
+}
