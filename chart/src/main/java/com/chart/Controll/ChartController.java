15ce7499d1e9882656835d59575890a6d862fa14
lama nawaraj <lama.nawraj00@gmail.com>
2022-02-04 08:34:54 +0900

    edit

--- chart/src/main/java/com/chart/Controll/ChartController.java
+++ chart/src/main/java/com/chart/Controll/ChartController.java
@@ -26,7 +26,7 @@ public class ChartController {
                 List.of("Onions", RANDOM.nextInt(5)),
                 List.of("Olives", RANDOM.nextInt(5)),
                 List.of("Zucchini", RANDOM.nextInt(5)),
-                List.of("Pepperment", RANDOM.nextInt(5)));
+                List.of("Pepperment", RANDOM.nextInt(5)),
                 List.of("Chillies", RANDOM.nextInt(5)));
     }
 
